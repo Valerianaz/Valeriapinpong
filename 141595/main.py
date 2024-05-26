9bc48c2ab78283efc01d04898f4afbb61a10a959
Valerianaz <133207342+Valerianaz@users.noreply.github.com>
2024-05-26 19:02:26 +0300

    я добавила счёт очков и проигрыш

--- 141595/main.py
+++ 141595/main.py
@@ -1,6 +1,9 @@
 from pygame import *
 from random import randint
 from time import time as timer
+score = 0
+font.init()
+font2 = font.SysFont('Arial', 45)
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y,player_speed,size_x,size_y):
         super().__init__()
@@ -13,13 +16,13 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 #класс описывающий ракетки
 class Player(GameSprite):
-    def update_r(self):
+    def update_l(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y >5 :
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-    def update_l(self):
+    def update_r(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y >5 :
             self.rect.y -= self.speed
@@ -39,18 +42,22 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
+background = transform.scale(image.load('background.jpg'), (win_width, win_height))
 #флаги, отвечающие за состояние игры
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
-#cоздание спрайтов
-racket1 = Player('racketka1.png',30,200,4,50,150)
-racket2 = Player('racketka1.png',520,200,4,50,150)
+#cоздание спрайтов/картинок
+racket1 = Player('racketka1.png',30,200,4,150,150)
+racket2 = Player('racketka1.png',490,200,4,150,150)
 ball = GameSprite('ball1.png',200,200,4,50,50)
-xx = 2
-yy = 2
+lost1 = GameSprite('New Piskel (6).png',200,175,0,200,150)
+lost2 = GameSprite('New Piskel (7).png',200,175,0,200,150)
+xx = 4
+yy = 4
 while game:
+    window.blit(background,(0,0))
     for e in event.get():
         if e.type == QUIT:
             game = False
@@ -60,15 +67,21 @@ while game:
         racket1.update_r()
         racket2.update_l()
         if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2,ball):
+            score+=1
             xx *= -1
             yy *= 1
-        if ball.rect.y > win_height -50 or ball.rect.y <0 :
-            yy += -1
-        if ball.rect.x < 0:
+        if ball.rect.y > win_height -50  or ball.rect.y <50 :
+            yy *= -1
+        if ball.rect.x < 50:
             finish = True
-            game_over = True
-    ball.reset()      
-    racket1.reset()
-    racket2.reset()
-    display.update()
+            lost1.reset()
+        if  ball.rect.x > win_width -50:
+            finish = True
+            lost2.reset()
+        text = font2.render('Твои очки:'+ str(score),1,(40,155,150))
+        window.blit(text,(10,20))
+        ball.reset()      
+        racket1.reset()
+        racket2.reset()
+        display.update()
     time.delay(30)
